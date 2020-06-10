1096f5ba712fe659994f2b640f49e996e3d0559b
Shubham Gupta <shubham15gupta09@gmail.com>
2020-06-10 23:36:45 +0530

    Bug - fix added the blank trigger

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 app
     .use(express.static('public'))
     .set('view engine', 'ejs')
-    .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.json({}))
     .get("/", (req, res) => {
         res.status(200).render("home.ejs");
--- public/fetch.js
+++ public/fetch.js
@@ -4,21 +4,25 @@ const APP_KEY = "37b650a4e558dc71340ea7a5e1b9d92d";
 const div = document.getElementById("content");
 const dish = document.getElementById('dish');
 const fetch_details = () => {
-    div.innerHTML = ``;
-    div.innerHTML = `Please wait while we fetch Recipe related with "${dish.value}"`;
-    fetch(`https://api.edamam.com/search?q=${dish.value}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`)
-        .then(res => res.json())
-        .then(recipe => {
-            div.innerHTML = ``;
-            const data = recipe.hits;
-            data.map(recipe => {
-                div.innerHTML +=
-                    `<img src=${recipe.recipe.image} style="margine:3px;padding:3px;item-align:center;width:100px;height:100px;"/><br>` +
-                    `<b>Dish : </b>${recipe.recipe.label}<br>` +
-                    `<b>Type : </b>${recipe.recipe.healthLabels[0]}<br>` +
-                    `<b>Calories : </b>${Math.floor(recipe.recipe.calories)}<br><hr>`;
-                dish.value = ``;
+    if (dish.value === "") {
+        alert("Oh ho seems that No input is Given .\nPlease try Again");
+    } else {
+        div.innerHTML = ``;
+        div.innerHTML = `Please wait while we fetch Recipe related with "${dish.value}"`;
+        fetch(`https://api.edamam.com/search?q=${dish.value}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`)
+            .then(res => res.json())
+            .then(recipe => {
+                div.innerHTML = ``;
+                const data = recipe.hits;
+                data.map(recipe => {
+                    div.innerHTML +=
+                        `<img src=${recipe.recipe.image} style="margine:3px;padding:3px;item-align:center;width:100px;height:100px;"/><br>` +
+                        `<b>Dish : </b>${recipe.recipe.label}<br>` +
+                        `<b>Type : </b>${recipe.recipe.healthLabels[0]}<br>` +
+                        `<b>Calories : </b>${Math.floor(recipe.recipe.calories)}<br><hr>`;
+                    dish.value = ``;
+                })
             })
-        })
-        .catch(err => console.log(err));
+            .catch(err => console.log(err));
+    }
 }
